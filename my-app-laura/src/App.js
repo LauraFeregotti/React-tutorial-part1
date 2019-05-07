a31c2eb94858e890c8b543aba152794502748520
unknown <laura.feregotti@gmail.com>
2019-05-07 17:57:36 +0200

    adding the project

---
+++ my-app-laura/src/App.js
@@ -0,0 +1,16 @@
+    
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Ninjas name="Ryu" age="25" belt="black" />
+      </div>
+    );
+  }
+}
+
+export default App;

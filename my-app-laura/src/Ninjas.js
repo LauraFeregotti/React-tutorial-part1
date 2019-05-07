32d2b42a6296df45e8e6d6ebe99c62de724b615e
unknown <laura.feregotti@gmail.com>
2019-05-07 23:29:11 +0200

    components- simplifying the UI component

--- my-app-laura/src/Ninjas.js
+++ my-app-laura/src/Ninjas.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-const Ninjas = (props) =>{ 
-   const {ninjas} = props;
+const Ninjas = ({ninjas}) =>{ 
+   
    const ninjaList = ninjas.map(ninja =>{
     return (
       <div className="ninja" key = {ninja.id}  >
